0a4f61ea617f7c9fe93c3b6b57f7295165e48681
Victor Torres <torres.victor136@gmail.com>
2020-08-10 17:47:40 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
   def parrot(default = "Squak!")
-    puts "#{(default)"
+    puts "#x(default)"
     "#(default)"
   end
